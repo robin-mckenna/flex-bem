a1b4bdeac43c4680c180f86e6f5932f8382874d8
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-11-11 09:15:45 +0000

    Update gulpfile to compile scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
 
 gulp.task('start', function() {
     console.log("Hello, starting...");
+});
+
+gulp.task('sass', function () {
+return gulp
+  .src('./stylesheets/scss/*.scss')
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(gulp.dest('./public/css'));
 });
